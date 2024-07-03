f8659f2b24908dc8f73e6a7922fe776694e06dea
Sharp0802 <sharp0802.business@gmail.com>
2024-07-03 20:17:47 +0900

    :construction: Include fundamental headers

--- tron/pch.h
+++ tron/pch.h
@@ -1,3 +1,29 @@
 #pragma once
 
+#define GLM_CONFIG_SWIZZLE GLM_SWIZZLE_FUNCTION
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
 #include <iostream>
+#include <limits>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <map>
+#include <vector>
+#include <utility>
+#include <ranges>
