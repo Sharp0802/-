7d2f98dcb6333e46a95018f3c0555a0e653bb254
Sharp0802 <sharp0802.business@gmail.com>
2024-07-06 19:00:17 +0900

    :wrench: Enable auto-detected clang when only detected compiler is not clang

--- cmake/useClang.cmake
+++ cmake/useClang.cmake
@@ -1,5 +1,7 @@
-find_program(CLANG_C_PATH clang)
-set(CMAKE_C_COMPILER ${CLANG_C_PATH})
+if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    find_program(CLANG_C_PATH clang)
+    set(CMAKE_C_COMPILER ${CLANG_C_PATH})
 
-find_program(CLANG_CXX_PATH clang++)
-set(CMAKE_CXX_COMPILER ${CLANG_CXX_PATH})
+    find_program(CLANG_CXX_PATH clang++)
+    set(CMAKE_CXX_COMPILER ${CLANG_CXX_PATH})
+endif ()
