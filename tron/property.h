fa97b3577f8841a40236d735e58c2803316d87a1
Sharp0802 <sharp0802.business@gmail.com>
2024-07-08 23:04:10 +0900

    :sparkles: Create macros for properties

---
+++ tron/property.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define PROP_GETTER_NAME(name) _##name##__getter_
+#define PROP_SETTER_NAME(name) _##name##__getter_
+
+#define PROP_RW(t, name) __declspec(property(get=PROP_GETTER_NAME(name), put=PROP_SETTER_NAME(name))) t name;
+#define PROP_RO(t, name) __declspec(property(get=PROP_GETTER_NAME(name))) t name;
+
+#define PROP_GETTER(name) decltype(name) PROP_GETTER_NAME(name)()
+#define PROP_SETTER(name) void PROP_SETTER_NAME(name)(decltype(name) value)
+
+#define PROP_AUTO_FIELD(name) _##name##__field_
+#define PROP_AUTO(vis, t, name) \
+    vis: \
+        PROP_RW(t, name) \
+    private: \
+        decltype(name) PROP_AUTO_FIELD(name); \
+    vis: \
+        PROP_GETTER(name) const { return PROP_AUTO_FIELD(name); } \
+        PROP_SETTER(name) { PROP_AUTO_FIELD(name) = value; }
